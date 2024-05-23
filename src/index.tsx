477e38204cb3ab08db09ffeff08f248209086f67
relativity-codes <ukweheverest@gmail.com>
2024-05-23 10:56:50 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,5 @@ import ReactEditorJSClient from "./client/ReactEditorJSClient";
 import { WrapperProps } from "./core";
 
 export default function CreateReactEditorJS(props: WrapperProps): React.ReactNode {
-  return <>{typeof window !== 'undefined' && <ReactEditorJSClient {...props} />}</>
+  return <ReactEditorJSClient {...props} />
 }
